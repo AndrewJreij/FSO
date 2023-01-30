4ce9d741694a9999e071ee276575390f7b9ebe5a
Andrew Jreij <andrew.jreij@gmail.com>
2023-01-29 19:16:16 -0500

    remove par3

--- src/App.js
+++ src/App.js
@@ -1,60 +1,113 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Country from './components/Country'
-import CountryList from './components/CountryList';
 
-const MAX_COUNTRY_RESULTS = 10;
-const ResultsWarning = () => <p>Too many matches, specify another filter</p>;
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
+
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+        </div>
+    )
+}
 
 const App = () => {
-    const [countries, setCountries] = useState();
-    const [filter, setFilter] = useState('');
-    const [filteredCountries, setFilteredCountries] = useState([]);
-    const [selectedCountry, setSelectedCountry] = useState();
-    const [fetchCountriesError, setFetchCountriesError] = useState();
-
-    const isCountryListVisible = filteredCountries.length < MAX_COUNTRY_RESULTS;
+    const [notes, setNotes] = useState(null)
+    const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     useEffect(() => {
-        axios
-            .get('https://restcountries.com/v3.1/all')
-            .then(({ data }) => setCountries(data))
-            .catch(err => setFetchCountriesError(err));
+        noteService
+            .getAll()
+            .then(initialNotes => {
+                setNotes(initialNotes)
+            })
     }, [])
 
-    useEffect(() => {
-        if (filter === '') return setFilteredCountries([]);
 
-        setFilteredCountries(countries.filter(country => country.name.common.toLowerCase().includes(filter)));
-    }, [filter, countries])
-
-    const handleFilterChange = (event) => {
-        setFilter(event.target.value)
+    if (!notes) {
+        return null
     }
 
-    const handleShowCountryClicked = (country) => {
-        setSelectedCountry(country);
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
+        }
+
+        noteService
+            .create(noteObject)
+            .then(returnedNote => {
+                setNotes(notes.concat(returnedNote))
+                setNewNote('')
+            })
     }
 
-    if (!countries) {
-        return <div>Loading</div>
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
     }
 
-    if (fetchCountriesError)
-        return <div>Error fetching countries</div>
+    const toggleImportanceOf = id => {
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        noteService
+            .update(id, changedNote)
+            .then(returnedNote => {
+                setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+            })
+            .catch(error => {
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
+                )
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+                setNotes(notes.filter(n => n.id !== id))
+            })
+    }
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
 
     return (
         <div>
+            <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
-                <input onChange={handleFilterChange} value={filter}></input>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
             </div>
-            <div>
-                {isCountryListVisible
-                    ? <CountryList data={filteredCountries} onClickShowCountry={handleShowCountryClicked} />
-                    : <ResultsWarning />
-                }
-                {selectedCountry && <Country country={selectedCountry} />}
-            </div>
+            <ul>
+                {notesToShow.map(note =>
+                    <Note
+                        key={note.id}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">save</button>
+            </form>
+            <Footer />
         </div>
     )
 }
