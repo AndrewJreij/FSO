cd3b8595a49a589ccff3828d1e5b2033c1544456
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-23 00:09:47 -0500

    added error messages to frontend when adding notes

--- src/App.js
+++ src/App.js
@@ -98,6 +98,12 @@ const App = () => {
                 setNotes(notes.concat(returnedNote))
                 setNewNote('')
             })
+            .catch(error => {
+                setErrorMessage(error.response.data.error)
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+            })
     }
 
     const handleNoteChange = (event) => {
