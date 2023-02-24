4f2c2a9b03c6d7bfae2e08101656a13631ed68d7
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-23 23:52:14 -0500

    5.2 3 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState()
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const [message, setMessage] = useState('')
 
 
   useEffect(() => {
@@ -24,26 +31,59 @@ const App = () => {
       setBlogs(blogs)
     )
       .catch(error => {
-        console.log(error.response.data)
+        console.log(error.response.data.error)
       })
   }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
 
-    const user = await loginService.login({ username, password })
+    try {
+      const user = await loginService.login({ username, password })
 
-    window.localStorage.setItem('loggedBlostlistUser', JSON.stringify(user))
-    setUser(user)
-    blogService.setToken(user.token)
-    setUsername('')
-    setPassword('')
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
+      setUser(user)
+      blogService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setMessage(error.response.data.error)
+      setTimeout(() => setMessage(null), 5000)
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    setUser(null)
+    window.localStorage.removeItem('loggedBloglistUser')
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+      blogs.concat(newBlog)
+
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+      setTimeout(() => setMessage(`new blog ${newBlog.title} by ${newBlog.author} added`), 5000)
+
+
+    } catch (error) {
+      setMessage(error.response.data.error)
+      setTimeout(() => setMessage(null), 5000)
+    }
   }
 
   if (!user) {
     return (
       <>
         <h1>Login to application</h1>
+        <Notification message={message} />
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -73,8 +113,47 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
+      {message}
+
       <div>
         {user.name} is logged in
+
+        <button onClick={handleLogout}>logout</button>
+      </div>
+
+      <div>
+        <h1>create new</h1>
+
+        <form onSubmit={handleNewBlog}>
+          <div>
+            title
+            <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
       </div>
 
 
