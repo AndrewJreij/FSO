b6b9bd901521992f5b35a1261ccafe14c1ef8bcb
Andrew Jreij <andrew.jreij@gmail.com>
2023-02-23 23:08:32 -0500

    5.1 .2

---
+++ src/services/blogs.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token
+
+const getAll = () => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const request = axios.get(baseUrl, config)
+  return request.then(response => response.data)
+}
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+  console.log(token)
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, setToken }
