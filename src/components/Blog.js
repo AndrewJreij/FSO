f7515d8c77e7252546735b72beb0fafdf1469ca4
Andrew Jreij <andrew.jreij@gmail.com>
2023-03-03 00:36:54 -0500

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,45 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const hideWhenDetails = { display: showDetails ? 'none' : '' }
+  const showWhenDetails = { display: showDetails ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} {showDetails}
+        <button style={hideWhenDetails} onClick={toggleDetails}>view</button>
+        <button style={showWhenDetails} onClick={toggleDetails}>hide</button>
+      </div>
+      <div style={showWhenDetails}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
